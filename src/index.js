1de978e39a8cc5fb501797600631ec33aac032f6
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2022-01-26 12:31:12 +0200

    feat: function towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  if (matrix.length === 0) {
+    return [];
+  }
+  let newArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i === 0 || i % 2 === 0) {
+     newArr.push(matrix[i])
+    } else {
+      newArr.push(matrix[i].reverse())
+    }
+    // console.log(matrix[i]);
+  }
+  let mergedArr = [].concat.apply([], newArr);
+  return mergedArr;
 }
